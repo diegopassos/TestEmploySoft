e961b918108978383df4a76ed87894951b0a58ce
Diego <Diego@LENOVO-Z40>
2017-03-13 09:36:55 +1000

    adjustment on dropdown list users

--- Like.Web/App/js/controllers/indexCtrl.js
+++ Like.Web/App/js/controllers/indexCtrl.js
@@ -2,9 +2,11 @@
 
     userAPI.getUserList().success(function (data) {
         $scope.userList = data;
+        $scope.userSelected = {Id: 1}
     });
 
     $rootScope.idUser = 1;
+
 
     $scope.selectUser = function (id) {
 
